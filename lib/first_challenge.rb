b17533d603953f1a511fe63821f1a018877d453a
Sarah Elzinga <sevans924@gmail.com>
2019-03-24 23:29:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -13,10 +13,18 @@ def first_challenge
     }
   }
 
-  #your code here
+contacts.each do |person, data|
+
+  data.each do |attribute, value|
+
+      value.delete_if |icecream, flavor|
+      flavor == "strawberry"
+    end
+  end
+end
+          #your code here
 
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
